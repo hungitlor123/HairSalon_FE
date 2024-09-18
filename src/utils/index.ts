8689341029ba32a27e4cee88e5e81148132d6845
NhutMinhIT <nhutminh.it19@gmail.com>
2024-09-18 22:53:40 +0700

    feat: fortmat date and number

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import moment from 'moment';
+
+export const formatAnyDate = (date: string | Date | null | undefined): string | null => {
+    return date ? moment.utc(date).format('DD-MM-YYYY') : null;
+}
+
+export const formatMMDDYYYYDate = (date: Date | null | undefined): string | null => {
+    return date ? moment(date).format('MM-DD-YYYY') : null;
+}
+
+export const formatNumberWithDots = (number: number | null | undefined): string | null => {
+    if (number === null || number === undefined) {
+        return null;
+    }
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+};
