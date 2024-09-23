e6e48776668ef9814f17b014ffe44a0b278163b5
hungdz369@gmail.com <hungdz369@gmail.com>
2024-09-23 23:03:31 +0700

    refactor forgotpage

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,8 +1,8 @@
-import ForgotPasswordForm from '@/components/authentication/ForgotPasswordForm'
-import Home from '@/page/customer/home/Homepage'
-import LoginPage from '@/page/customer/Login/LoginPage'
-import RegisterPage from '@/page/customer/Register/RegisterPage'
+import Home from '@/page/customer/Home/Homepage'
+import LoginPage from '@/page/Auth/Login/LoginPage'
+import RegisterPage from '@/page/Auth/Register/RegisterPage'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import ForgotPassWordPage from '@/page/Auth/ForgotPassWord/ForgotPassWordPage'
 const AppRouter = () => {
     const token = sessionStorage.getItem("hairSalon")
     return (
@@ -11,7 +11,7 @@ const AppRouter = () => {
                 <>
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
-                    <Route path='/forgot_password' element={<ForgotPasswordForm />} />
+                    <Route path='/forgot_password' element={<ForgotPassWordPage />} />
                     <Route path="*" element={<Navigate to="/" replace />} />
                     <Route path='/' element={<Home />} />
                 </>
