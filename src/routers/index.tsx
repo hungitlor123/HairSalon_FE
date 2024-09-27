eab198a43ff5f6ad82f5a4a5460bd7d4568a7f20
ThanhTai1309 <tai73559@gmail.com>
2024-09-27 21:11:31 +0700

    feat : authentication page

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -4,9 +4,11 @@ import RegisterPage from '@/page/auth/Register/RegisterPage'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ForgotPassWordPage from '@/page/auth/ForgotPassWord/ForgotPassWordPage'
 import Profile from '@/page/customer/home/Profilepage'
+import { useAppSelector } from '@/services/store/store'
 const AppRouter = () => {
-    const token = sessionStorage.getItem("hairSalon")
-    
+    const token = sessionStorage.getItem("hairSalon");
+    const { auth } = useAppSelector((state) => state.auth);
+    const isCustomer = auth?.roleId === "R4"
     return (
         <Routes>
             {token === null ? (
@@ -14,12 +16,17 @@ const AppRouter = () => {
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
                     <Route path='/forgot_password' element={<ForgotPassWordPage />} />
-                    <Route path='/profile' element={<Profile />} />
                     <Route path="*" element={<Navigate to="/" replace />} />
                     <Route path='/' element={<Home />} />
                 </>
             ) : (
                 <>
+                    <Route path='/profile' element={<Profile />} />
+                    {isCustomer && (
+                        <>
+                            <Route path='/' element={<Home />} />
+                        </>
+                    )}
                 </>
             )}
 
