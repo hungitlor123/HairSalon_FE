41847c0eb38375d96042f6b0ff86d0a4632a6f79
NhutMinhIT <nhutminh.it19@gmail.com>
2024-09-28 08:27:54 +0700

    feat: axiosInstance refresh token

--- src/services/constant/axiosInstance.ts
+++ src/services/constant/axiosInstance.ts
@@ -1,2 +1,43 @@
+import axios from 'axios';
+import { store } from './../store/store';
+import { BASE_URL } from './apiConfig';
+import { toast } from 'react-toastify';
+import { logoutUser, refreshAccessToken } from '../features/auth/authSlice';
 
-// Waiting API
+const axiosInstance = axios.create({
+    baseURL: BASE_URL
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = sessionStorage.getItem('hairSalonToken');
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const newAccessToken = await store.dispatch(refreshAccessToken()).unwrap();
+                axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+                return axiosInstance(originalRequest);
+            } catch (err) {
+                store.dispatch(logoutUser());
+                window.location.href = '/login';
+                toast.error("Hết hạn đăng nhập. Vui lòng đăng nhập lại!");
+                return Promise.reject(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
